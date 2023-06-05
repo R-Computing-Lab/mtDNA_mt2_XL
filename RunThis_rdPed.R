f4667f9174ebc6dd243a6909355f5732ad631700
Xuanyu Lyu <lxy750909935@gmail.com>
2023-06-05 10:21:19 -0400

    rdped20k c4 results

--- RunThis_rdPed.R
+++ RunThis_rdPed.R
@@ -3,12 +3,12 @@
 source("~/R-Project/mtDNA_mt2/InitialData/InitialDataPrep_R2.R")
 source("~/R-Project/mtDNA_mt2/Functions/RunSim_rdped.R")
 
-for(i in 6:11){
+for(i in 4){
     
     for(j in 1: 10){
         print(Sys.time())
         cat(paste("start c",i,"p",j, "\n"))
-        target_folder <- paste0("~/R-Project/mtDNA_mt2/Result_rdped","/","c",i,"p",j) 
+        target_folder <- paste0("~/R-Project/mtDNA_mt2/Result_rdped20k","/","c",i,"p",j) 
         if (!dir.exists(target_folder)){
             dir.create(target_folder)
         }
@@ -20,7 +20,7 @@ for(i in 6:11){
                   Ngen = df_ped$G[j],
                   sexR = df_ped$p[j],
                   marR = df_ped$r[j],
-                  n = 10000,
+                  n = 20000,
                   path_results = target_folder)
         print(Sys.time())
         cat(paste("end c",i,"p",j, "\n"))
